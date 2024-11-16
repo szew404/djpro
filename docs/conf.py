6693ccdd59efa389d1563a46b78328e2b5a4ab8e
gidaszewski <gidaszewskifranco@gmail.com>
2024-11-16 13:22:13 +0100

    docs theme changed

--- docs/conf.py
+++ docs/conf.py
@@ -6,23 +6,22 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'dj-pro'
-copyright = '2024, Franco Gidaszewski'
-author = 'Franco Gidaszewski'
-release = '0.0.1'
+project = "dj-pro"
+copyright = "2024, Franco Gidaszewski"
+author = "Franco Gidaszewski"
+release = "0.0.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "furo"
+html_static_path = ["_static"]
