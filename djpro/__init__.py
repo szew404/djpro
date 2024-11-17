014cabc22323ea71e5d109ddce4bf02379a6ab53
gidaszewski <gidaszewskifranco@gmail.com>
2024-11-17 15:43:44 +0100

    update version

--- djpro/__init__.py
+++ djpro/__init__.py
@@ -17,7 +17,7 @@ from .base import *
 __copyright__ = "Copyright (c) 2024-2034, Franco Gidaszewski"
 """The copyright notice of the package."""
 
-__version__ = "0.0.1-BETA"
+__version__ = "0.0.2"
 """The version of the package."""
 
 __license__ = "MIT"
