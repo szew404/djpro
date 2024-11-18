668d631ef58c9a301186fc0523c1022dc8d3c4ab
gidaszewski <gidaszewskifranco@gmail.com>
2024-11-18 12:58:09 +0100

    fix docs conf

--- docs/conf.py
+++ docs/conf.py
@@ -53,7 +53,6 @@ extensions = [
     "sphinx.ext.intersphinx",
     "sphinx.ext.todo",
     "sphinx.ext.viewcode",
-    "notfound.extension",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
