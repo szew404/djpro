01242da8cd389f13162c83be6a809ccf4bdbdff4
gidaszewski <gidaszewskifranco@gmail.com>
2024-12-16 11:44:25 +0100

    Fix import in the content of wsgi file

--- djpro/utils/files_content.py
+++ djpro/utils/files_content.py
@@ -584,6 +584,7 @@ wsgi_content = """# WSGI config for {project_name} project.
 # https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
